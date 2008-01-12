0b3e414812fa00e36e445d6c0c8276179814304a
garionPHX <garionPHX@dbfa3ea4-2943-0410-9ce2-c73cf035c932>
2008-01-12 00:17:15 +0000

    Commit changed to the FrontPython controller

--- trunk/FrontPython.py
+++ trunk/FrontPython.py
@@ -6,50 +6,21 @@
 #  Copyright __MyCompanyName__ 2007. All rights reserved.
 #
 
+
 #import modules required by application
-import objc
-import Foundation
-import AppKit
+import PyFR.Appliance
+import PyFR.WaitController
 
-# Import backrow
-objc.loadBundle("BackRow", globals(), bundle_path=objc.pathForFramework("/System/Library/PrivateFrameworks/BackRow.framework" ))
+class MyWaitController(PyFR.WaitController.WaitController):
+    def PyFR_start(self):
+        self.launchApp( '/Applications/MythFrontend.app')
 
-def log(s):
-    Foundation.NSLog( "FrontPython: %s" % str(s) )
+class RUIPythonAppliance( PyFR.Appliance.Appliance ):
+    def getController(self):
+        return MyWaitController.alloc().initWithText_("Loading Myth")
 
 
-key = u"com.tzo.garion.frontpython"
 
-class RUIPythonAppliance( BRAppliance ):
 
-    sanityCheck = False
-
-    @classmethod
-    def initialize(cls):
-        name = NSString.alloc().initWithString_( u"com.apple.frontrow.appliance.frontpython" )
-        BRFeatureManager.sharedInstance().enableFeatureNamed_( name )
-
-    @classmethod
-    def className(cls):
-
-        clsName = NSString.alloc().initWithString_( cls.__name__ )
-
-        backtrace = BRBacktracingException.backtrace()
-        range = backtrace.rangeOfString_( "_loadApplianceInfoAtPath:" )
-
-        if range.location == Foundation.NSNotFound and cls.sanityCheck == False:
-            range = backtrace.rangeOfString_( "(in BackRow)" )
-            cls.sanityCheck = True
-        
-        if range.location != Foundation.NSNotFound:
-            clsName = NSString.alloc().initWithString_( "RUIMoviesAppliance" )
-
-        return clsName
-
-    def applianceController(self):
-
-        alert = BRAlertController.alertOfType_titled_primaryText_secondaryText_( 0, "Title", "Hello", "world" )
-
-        return alert
 
 

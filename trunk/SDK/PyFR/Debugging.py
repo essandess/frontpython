49a54cbca994e254eab4265f816f753626661a72
jon.christopher <jon.christopher@dbfa3ea4-2943-0410-9ce2-c73cf035c932>
2008-01-14 08:27:56 +0000

    adding ability to enable logging of all objective-c functions

--- trunk/SDK/PyFR/Debugging.py
+++ trunk/SDK/PyFR/Debugging.py
@@ -1,5 +1,6 @@
 import traceback
-
+import os
+import objc
 import Foundation
 import PyObjCTools.Debugging
 
@@ -22,4 +23,15 @@ def PyFRExceptionLogger(exception):
 PyObjCTools.Debugging.nsLogPythonException = PyFRExceptionLogger
 
 PyObjCTools.Debugging.installPythonExceptionHandler()
+
+#
+# call this function to enable logging of all objective-c calls to /tmp/msgSends-<pid> while /tmp/FRLOG file exists
+#
+objc.loadBundleFunctions(Foundation.__bundle__, globals(),[('instrumentObjcMessageSends', objc._C_VOID + objc._C_NSBOOL)])
+class EnableObjcLogger():
+    def __init__(self):
+        self.timer = Foundation.NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_( 0.25, self, "tick:", None, True )
+        
+    def tick_(self, senders):
+        instrumentObjcMessageSends(os.path.isfile("/tmp/FRLOG"))
 

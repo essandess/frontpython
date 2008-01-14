6b478130ab93d8b7e9f2a72c677427937789c77e
jon.christopher <jon.christopher@dbfa3ea4-2943-0410-9ce2-c73cf035c932>
2008-01-14 08:17:30 +0000

    adding imports to allow simple "import PyFR" in app to get everything

--- trunk/SDK/PyFR/__init__.py
+++ trunk/SDK/PyFR/__init__.py
@@ -1,1 +1,9 @@
+#__all__ = [ "BackRow", "Appliance", "MenuController", "OptionDialog", "Utilities", "WaitController", "Debugging" ]
 
+from Appliance import *
+from BackRow import *
+from MenuController import *
+from OptionDialog import *
+from Utilities import *
+from WaitController import *
+from Debugging import *
